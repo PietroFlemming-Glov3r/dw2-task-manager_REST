36aed6cb56a540bfcce6431b4276be7a45be2458
Pietro Flemming <pietroflemming@gmail.com>
2025-01-30 13:10:14 -0300

    commit .

--- tasks/views.py
+++ tasks/views.py
@@ -8,6 +8,7 @@ class TaskViewSet(viewsets.ModelViewSet):
     queryset = task.objects.all()
     serializer_class = TaskSerializer
 
+    # Não sei se isso vai funcionar
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
