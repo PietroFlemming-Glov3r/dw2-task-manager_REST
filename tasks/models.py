3ca658c1a8866eab9d185705b66d7889f42ba5d1
Pietro Flemming <pietroflemming@gmail.com>
2025-01-30 12:09:20 -0300

    commit adding models, serializers and views for the tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+class task(models.Model):
+    task_name = models.CharField(max_length=100)
+    task_description = models.TextField()
+    TAGS_CHOICES = [ ('trabalho', 'Trabalho'), ('pessoal', 'Pessoal'), ('estudo', 'Estudo'), ('outro', 'Outro') ]
+    tags = models.CharField(max_length=15, choices=TAGS_CHOICES, default='outro')
+    task_done = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.task_name
