798cb0bb7886b96322ae148a035655218a10739b
Pietro Flemming <pietroflemming@gmail.com>
2025-02-02 00:03:03 -0300

    commit arrumei denovo

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
-    path("", include("tasks.urls")),  # Inclui as rotas do app tasks
+    path("", include("tasks.urls")),
 ]
