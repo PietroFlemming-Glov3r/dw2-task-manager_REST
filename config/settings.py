2322f89f56e277bae3f0eba13fb22b39afb69cfe
Pietro Flemming <pietroflemming@gmail.com>
2025-02-03 16:59:09 -0300

    commit removendo itens desnecessários

--- config/settings.py
+++ config/settings.py
@@ -42,11 +42,6 @@ INSTALLED_APPS = [
     'home',
     'rest_framework',
     'rest_framework.authtoken',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'dj_rest_auth',   
-    'dj_rest_auth.registration'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +52,6 @@ MIDDLEWARE = [
 'django.contrib.auth.middleware.AuthenticationMiddleware',
 'django.contrib.messages.middleware.MessageMiddleware',
 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,4 @@ urlpatterns = [
     path("", include("home.urls")),
     path('tasks/', include('tasks.urls', namespace='tasks')),
     path('api-auth/', include('rest_framework.urls')),
-    path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
